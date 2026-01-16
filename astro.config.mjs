f73b28de74f01ca5724d1fad152286a0aa278562
sfr <steve.fragniere@oiken.ch>
2026-01-16 09:06:24 +0100

    Astro + Netlify ready build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify'; // <-- ajouté
 
 export default defineConfig({
+  output: 'server',       // nécessaire pour Netlify adapter
+  adapter: netlify(),     // <-- ajouté
   integrations: [tailwind()],
 });
