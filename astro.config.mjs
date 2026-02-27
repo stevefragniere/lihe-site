a76bef0925dcd641bfe66ba97817973bc52a7c29
sfr <steve.fragniere@oiken.ch>
2026-02-27 09:07:12 +0100

    Add sitemap for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify'; // <-- ajouté
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'server',       // nécessaire pour Netlify adapter
   adapter: netlify(),     // <-- ajouté
-  integrations: [tailwind()],
+  integrations: [tailwind(),sitemap()],
+  site: 'https://lihe.ch',
+
 });
